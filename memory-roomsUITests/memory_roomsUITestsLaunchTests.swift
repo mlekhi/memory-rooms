33521914de48d1185b490c420d3732b979ce0b58
mlekhi <maya.lekhi1@gmail.com>
2025-01-15 18:36:20 -0500

    Initial Commit

---
+++ memory-roomsUITests/memory_roomsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  memory_roomsUITestsLaunchTests.swift
+//  memory-roomsUITests
+//
+//  Created by Maya Lekhi on 2025-01-15.
+//
+
+import XCTest
+
+final class memory_roomsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
